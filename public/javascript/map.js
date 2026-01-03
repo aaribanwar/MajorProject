b328b9ea202054636d5e73e0e270ce07ff4539d6
aaribanwar <aaribactivism@gmail.com>
2026-01-03 10:13:02 +0530

    WIP: maplibre integration debugging

---
+++ public/javascript/map.js
@@ -0,0 +1,9 @@
+console.log("map.js executed LOLOLOOOOL");
+
+
+const map = new maplibregl.Map({
+  container: "map",
+  style: "https://demotiles.maplibre.org/style.json",
+  center: [77.5946, 12.9716],
+  zoom: 12
+});
